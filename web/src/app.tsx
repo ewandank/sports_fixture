5ef97778e493ca3933522ff29b307c2da6ac8b4d
Ewan Dankesreither <ejdankesreither@gmail.com>
2026-07-13 20:02:20 +1000

    icons!

--- web/src/app.tsx
+++ web/src/app.tsx
@@ -1,14 +1,16 @@
 import { Button } from "./components/button";
-import { Checkbox } from "./components/checkbox";
 import { Item } from "./components/item";
+import caIcon from "./assets/ca.png";
+import mfcIcon from "./assets/mfc.png";
+import starsIcon from "./assets/stars.png";
 
 export function App() {
   return (
-    <div class="flex flex-col p-2 gap-4">
+    <div class="flex flex-col p-2 gap-3">
       <h1 class="text-4xl">Fixtures</h1>
       <p>Too much sport is barely enough.</p>
       <form
-        className="flex flex-col gap-4"
+        className="flex flex-col gap-2.5"
         onSubmit={(e) => {
           e.preventDefault();
 
@@ -16,15 +18,15 @@ export function App() {
           console.log(data);
         }}
       >
-        <Item name="Australian Women's Cricket" />
-        <Item name="Australian Men's Cricket" />
-        <Item name="Melbourne Demons Men" />
-        <Item name="Melbourne Demons Women" />
-        <Item name="Melbourne Stars Men" />
-        <Item name="Melbourne Stars Women" />
-        <Item name="Melbourne United (Coming Soon)" />
+        <Item name="Australian Women's Cricket" icon={caIcon} />
+        <Item name="Australian Men's Cricket" icon={caIcon} />
+        <Item name="Melbourne Demons Men" icon={mfcIcon} />
+        <Item name="Melbourne Demons Women" icon={mfcIcon} />
+        <Item name="Melbourne Stars Men" icon={starsIcon} />
+        <Item name="Melbourne Stars Women" icon={starsIcon} />
+        <Item name="Melbourne United (Coming Soon)" disabled />
 
-        <Button type="submit">Hello</Button>
+        <Button type="submit">Get me a calendar!</Button>
       </form>
     </div>
   );
