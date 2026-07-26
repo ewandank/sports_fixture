89a30b67aaec92ac7c50860b428e059c093e52c0
Ewan Dank <101682978+ewandank@users.noreply.github.com>
2026-07-26 23:06:29 +1000

    Fix Australia fixtures being too brief (#5)

--- api/handlers/aus_cricket_women.py
+++ api/handlers/aus_cricket_women.py
@@ -7,4 +7,4 @@ from utils.cricket import fetch_team_fixtures
 
 @register_option(Teams.AUS_CRICKET_WOMEN)
 async def get_fixture(client: AsyncCacheClient):
-    return await fetch_team_fixtures(client, team_id=68)
+    return await fetch_team_fixtures(client, team_id=68, include_game_str=True)
